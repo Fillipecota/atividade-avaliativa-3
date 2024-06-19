f5caad047fc3d6f03df2e3b810e429312ad78d35
FILLIPE SANT'ANNA COTA <filipe_s_cota@estudante.sc.senai.br>
2024-06-18 21:54:34 -0300

    prova

---
+++ src/questao05.js
@@ -0,0 +1,15 @@
+import leia, { questionInt } from "readline-sync"
+
+var linha1 = leia.questionFloat(" DIGITE A MEDIDA DO PRIMEIRO LADO \n")
+var linha2 = leia.questionFloat("DIGITE A MEDIDA DO SEGUNDO LADO \ n")
+var linha3 = leia. questionFloat("DIGITE A MEDIDA DO TERCEIRO LADO \n")
+
+if(linha1 < (linha2 + linha3)){
+    if (linha2 < (linha1 + linha3)){
+         if (linha3< (linha1 + linha2)) {
+            
+         }
+    }
+
+   
+}
